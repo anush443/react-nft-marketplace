c6d5cab16b0ab163f9ab54b5bd1856457529b851
anushgs <anush443@gmail.com>
2022-09-09 18:55:34 +0530

    morlais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sell from "./pages/Sell";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sell" element={<Sell />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
