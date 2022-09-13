bcaf57783d2dcbe2d6c2071c9563c4bd80aed3a8
anushgs <anush443@gmail.com>
2022-09-13 17:21:40 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { ConnectButton } from "web3uikit";
+import { NavLink } from "reactstrap";
 
 const Navbar = () => {
   return (
-    <div>
-      <ConnectButton />
-    </div>
+    <>
+      <nav>
+        <div className="flex items-center justify-between px-3 py-2 border-b-2">
+          <div className="">
+            <h3 className="text-2xl bold ">NFTMarketplace</h3>
+          </div>
+          <div className="flex space-x-6  font-sans ">
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/sell">SellNft</NavLink>
+          </div>
+          <div className="">
+            <ConnectButton moralisAuth={false} />
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
