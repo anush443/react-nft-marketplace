33ff0c817473426d457081f65b8ea39fcecf07b7
anushgs <anush443@gmail.com>
2022-09-15 22:38:02 +0530

    ActiveItem cloud func

--- src/moralis/cloudFunctions/updateActiveItems.js
+++ src/moralis/cloudFunctions/updateActiveItems.js
@@ -1,1 +1,22 @@
-console.log("final2");
+Moralis.Cloud.afterSave("ItemListed", async function (request) {
+  const confirmed = request.object.get("confirmed");
+  const logger = Moralis.Cloud.getLogger();
+  logger.info("Looking for confirmed tx");
+  if (confirmed) {
+    logger.info("Found item");
+    const ActiveItem = Moralis.Object.extend("ActiveItem");
+    const activeItem = new ActiveItem();
+    activeItem.set("marketplaceAddress", request.object.get("address"));
+    activeItem.set("nftAddress", request.object.get("nftAddress"));
+    activeItem.set("tokenId", request.object.get("tokenId"));
+    activeItem.set("price", request.object.get("price"));
+    activeItem.set("seller", request.object.get("seller"));
+    logger.info(
+      `Adding Address: ${request.object.get(
+        "address"
+      )} TokenId: ${request.object.get("tokenId")}`
+    );
+    logger.info("Saving item...");
+    activeItem.save();
+  }
+});
