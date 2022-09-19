3991dd3cc076f0015502db8e1986304994ef1a0c
anushgs <anush443@gmail.com>
2022-09-19 17:29:54 +0530

    canceld item cloud func

--- src/moralis/cloudFunctions/updateActiveItems.js
+++ src/moralis/cloudFunctions/updateActiveItems.js
@@ -20,3 +20,32 @@ Moralis.Cloud.afterSave("ItemListed", async function (request) {
     activeItem.save();
   }
 });
+
+Moralis.Cloud.afterSave("ItemCancelled", async function (request) {
+  const confirmed = request.object.get("Confirmed");
+  const logger = Moralis.Cloud.getLogger();
+  logger.info(`Marketplace | object:${request.object}`);
+  if (confirmed) {
+    const ActiveItem = Moralis.Object.extend("ActiveItem");
+    const query = new Moralis.Query("ActiveItem");
+    query.equalTo("marketplaceAddress", request.object.get("address"));
+    query.equalTo("nftAddress", request.object.get("nftAddress"));
+    query.equalTo("tokenId", request.object.get("tokenId"));
+    logger.info(`Marketplace | query ${query}`);
+    const cancelledItem = await query.first();
+    if (cancelledItem) {
+      logger.info(
+        `Deleting ${request.object.get(
+          "tokenId"
+        )} at address ${request.object.get("address")} since it was cancelled `
+      );
+      await cancelledItem.destroy();
+    } else {
+      logger.info(
+        `No Item found with ${request.object.get(
+          "tokenId"
+        )} at address ${request.object.get("address")}`
+      );
+    }
+  }
+});
