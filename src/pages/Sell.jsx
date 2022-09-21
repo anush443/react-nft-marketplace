dae1956625eae63c03d1be88f472a51cb48fbd55
anushgs <anush443@gmail.com>
2022-09-21 22:58:51 +0530

    List

--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { Form } from "web3uikit";
 
 const Sell = () => {
-  return <div>Sell</div>;
+  const approveAndList = () => {};
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-3xl mt-16 mx-auto px-4 py-2 bg-slate-500 rounded-xl">
+        <Form
+          className="mx-auto"
+          title="List Your NFT"
+          onSubmit={approveAndList}
+          data={[
+            {
+              name: "Nft Address",
+              type: "text",
+              inputWidth: "50%",
+              value: "",
+              key: "nftAddress",
+            },
+            {
+              name: "Token Id",
+              type: "number",
+              inputWidth: "50%",
+              value: "",
+              key: "tokenId",
+            },
+            {
+              name: "Price",
+              type: "number",
+              inputWidth: "50%",
+              value: "",
+              key: "price",
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Sell;
