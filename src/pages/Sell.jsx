e45a8517b35ce3d715cbd3cd593bc844d3b41e3b
anushgs <anush443@gmail.com>
2022-09-21 23:24:13 +0530

    sell nft

--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -1,9 +1,83 @@
 import React from "react";
+import basicNftAbi from "../constants/BasicNft.json";
+import NftMarketplace from "../constants/NftMarketplace.json";
+import networkMapping from "../constants/networkMapping.json";
 import Navbar from "../components/Navbar";
-import { Form } from "web3uikit";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { Form, useNotification } from "web3uikit";
 
 const Sell = () => {
-  const approveAndList = () => {};
+  const { chainId } = useMoralis();
+  const chainIdString = chainId ? parseInt(chainId) : "31337";
+
+  const marketplaceAddress = networkMapping[chainIdString]["NftMarketplace"][0];
+  const { runContractFunction } = useWeb3Contract();
+
+  const dispatch = useNotification();
+
+  const handleSuccessOrErrorNotification = async (type, message) => {
+    dispatch({
+      type: type,
+      title: "Transaction Notification",
+      message: message,
+      position: "topR",
+      icon: "bell",
+    });
+  };
+
+  const handleSuccess = async (tx) => {
+    await tx.wait(1);
+
+    await handleSuccessOrErrorNotification("info", `Successfully listed nft`);
+  };
+
+  const handleError = async (error) => {
+    console.log(error);
+    handleSuccessOrErrorNotification("error", "Something went wrong");
+  };
+
+  const approveAndList = async (data) => {
+    console.log(data);
+    const nftAddress = data.data[0].inputResult;
+    const tokenId = data.data[1].inputResult;
+    const price = data.data[2].inputResult;
+
+    const approveOptions = {
+      abi: basicNftAbi,
+      contractAddress: nftAddress,
+      functionName: "approve",
+      params: {
+        to: marketplaceAddress,
+        tokenId: tokenId,
+      },
+    };
+
+    await runContractFunction({
+      params: approveOptions,
+      onSuccess: handleAprroveSuccess(nftAddress, tokenId, price),
+      onError: (error) => console.log(error),
+    });
+  };
+
+  const handleAprroveSuccess = async (nftAddress, tokenId, price) => {
+    const listOptions = {
+      abi: NftMarketplace,
+      contractAddress: marketplaceAddress,
+      functionName: "listItem",
+      params: {
+        nftAddress: nftAddress,
+        tokenId: tokenId,
+        price: price,
+      },
+    };
+
+    await runContractFunction({
+      params: listOptions,
+      onSuccess: handleSuccess,
+      onError: (error) => console.log(error),
+    });
+  };
+
   return (
     <>
       <Navbar />
